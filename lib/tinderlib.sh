c6ac584ac73d89225ce8d5ff47da3071ec98633f
marcus <marcus>
2005-06-15 19:30:51 +0000

    Big patch to add a more general mounting and unmounting procedure.  Now, all mounts are handled in such a way to avoid stale mounts, and so that news file system supported can be easily added.
    Submitted by:	oliver (with modifications)

---
+++ lib/tinderlib.sh
@@ -0,0 +1,223 @@
+kill_procs()
+{
+	dir=$1
+
+	pids="XXX"
+	while [ ! -z "${pids}" ]; do
+		pids=$(fstat -f "$dir" | tail +2 | awk '{print $3}' | sort -u)
+		if [ ! -z "${pids}" ]; then
+			echo "Killing off pids in ${dir}"
+			ps -p $pids
+			kill -KILL ${pids} 2> /dev/null
+			sleep 2
+		fi
+	done
+}
+
+cleanup_mount() {
+	mount=$1
+
+	if [ -d ${mount} ] ; then
+		if [ $(fstat -f ${mount} | wc -l) -gt 1 ] ; then
+			kill_procs ${mount}
+		fi
+		umount ${mount} || echo "Cleanup of ${chroot}${mount} failed!"
+	fi
+}
+
+cleanup_mounts() {
+
+	_build=
+	_jail=
+	_portstree=
+	_destination=
+
+	_ARCH=${ARCH:=$(uname -m)}
+
+	_pb=${pb:=/space}
+	# Just in case /space is a symlink
+	_pb=$(realpath ${_pb})
+
+	while getopts d:b:p:j: OPT ; do
+		case ${OPT} in
+			d)      _destination=${OPTARG}
+				;;
+			b)      _build=${OPTARG}
+				;;
+			p)      _portstree=${OPTARG}
+				;;
+			j)      _jail=${OPTARG}
+				;;
+		esac
+	done
+
+	case ${_destination} in
+                jail)
+                        if [ -z "${_jail}" ] ; then
+                                echo "jail has to be filled!"
+				return 1
+                        fi
+                        _destination=${_pb}/jails/${_jail}
+                        ;;
+                portstree)
+			if [ -z "${_portstree}" ] ; then
+				echo "portstree has to be filled!"
+				return 1
+			fi
+                        _destination=${_pb}/portstrees/${_portstree}
+                        ;;
+                build)
+			if [ -z "${_build}" ] ; then
+				echo "build has to be filled!"
+				return 1
+			fi
+                        _destination=${_pb}/${_build}
+			if [ "${_ARCH}" = "i386" ] ; then
+				umount -f ${_destination}/compat/linux/proc >/dev/null 2>&1
+			fi
+                        ;;
+		distcache)
+			if [ -z "${_build}" ] ; then
+				echo "build has to be filled!"
+				return 1
+			fi
+			_destination=${_pb}/${_build}/distcache
+			;;
+                *)      echo "unknown destination type!"
+			return 1
+                        ;;
+        esac
+
+	df | grep ' '${_destination}'[/$]' | sed 's|.* ||g' | sort -r | while read mountpoint ; do
+		cleanup_mount ${mountpoint}
+	done
+
+	return 0
+}
+
+request_mount() {
+
+	_source=
+	_destination=
+	_nullfs=0
+	_readonly=0
+	_build=
+	_jail=
+	_portstree=
+
+	_pb=${_pb:=/space}
+	_pb=$(realpath ${_pb})
+
+	_nullfs=0
+
+	while getopts nrs:d:b:p:j: OPT ; do
+		case ${OPT} in
+			n)	_nullfs=1
+				;;
+			r)	_readonly=1
+				;;
+			s)	_source=${OPTARG}
+				;;
+			d)	_destination=${OPTARG}
+				;;
+			b)	_build=${OPTARG}
+				;;
+			p)	_portstree=${OPTARG}
+				;;
+			j)	_jail=${OPTARG}
+				;;
+		esac
+	done
+
+	case ${_destination} in
+		jail)
+			if [ -z "${_jail}" ] ; then
+				echo "jail has to be filled!"
+				return 1
+			fi
+			_destination=${_pb}/jails/${_jail}/src
+			;;
+		portstree)
+			_destination=${_pb}/portstrees/${_portstree}/ports
+			;;
+		buildsrc)
+			if [ -z "${_build}" ] ; then
+				echo "build has to be filled!"
+				return 1
+			fi
+			_jail=$(${_pb}/scripts/tc getJailForBuild -b ${_build})
+			_source=${_source:=${_pb}/jails/${_jail}/src}
+			_destination=${_pb}/${_build}/usr/src
+			;;
+		buildports)
+			if [ -z "${_build}" ] ; then
+				echo "build has to be filled!"
+				return 1
+			fi
+			_portstree=$(${_pb}/scripts/tc getPortsTreeForBuild -b ${_build})
+			_source=${_source:=${_pb}/portstrees/${_portstree}/ports}
+			_destination=${_pb}/${_build}/a/ports
+			;;
+		distcache)
+			_destination=${_pb}/${_build}/distcache
+			;;
+		ccache)
+			;;
+		*)	echo "unknown destination type!"
+			return 1
+			;;
+	esac
+
+	if [ -z "${_source}" ] ; then
+		echo "source has to be filed!"
+		return 1
+	fi
+	if [ -z "${_destination}" ] ; then
+		echo "destination has to be filed!"
+		return 1
+	fi
+
+	# is _nullfs mount specified?
+	if [ ${_nullfs} -eq 1 ] ; then
+	    	if [ ${_readonly} -eq 1 ] ; then
+		    	mount -t nullfs -r ${_source} ${_destination}
+		else
+			mount -t nullfs ${_source} ${_destination}
+		fi
+	else # it has to be a nfs mount then
+		# lets check what kind of _source we have. If it is allready in
+		# a nfs format, we don't need to adjust anything
+		case ${_source} in
+			[a-zA-Z0-9\.-_]*:/*)
+				;;
+			*)
+				# find out the filesystem the requested source is in
+				filesystem=$(df ${_source} | awk '{a=$1}  END {print a}')
+				mountpoint=$(df ${_source} | awk '{a=$NF} END {print a}')
+				# determine if the filesystem the requested source
+				# is a nfs mount, or a local filesystem
+				case ${filesystem} in
+					[a-zA-Z0-9\.-_]*:/*)
+						# maybe our destination is a subdirectory of the mountpoint
+						# and not the mountpoint itself
+						# if that is the case, add the subdir to the mountpoint (sed)
+						_source="${filesystem}/$(echo $_source | sed 's|'${mountpoint}'||')"
+						;;
+					*)
+						# not a nfs mount, nullfs not specified - mount it as nfs
+						# from localhost
+						_source="localhost:/${_source}"
+						;;
+				esac
+				;;
+		esac
+
+		if [ ${_readonly} -eq 1 ] ; then
+			mount -o nfsv3,intr -r ${_source} ${_destination}
+		else
+		    	mount -o nfsv3,intr ${_source} ${_destination}
+		fi
+	fi
+
+	return 0
+}
