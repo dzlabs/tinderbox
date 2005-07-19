ed8655eafba257904442cf6b07cabf57d1e23539
marcus <marcus>
2005-07-19 07:19:27 +0000

    Perltidy.

--- lib/MakeCache.pm
+++ lib/MakeCache.pm
@@ -22,7 +22,7 @@
 # OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 # SUCH DAMAGE.
 #
-# $MCom: portstools/tinderbox/lib/MakeCache.pm,v 1.1 2005/07/19 07:18:20 marcus Exp $
+# $MCom: portstools/tinderbox/lib/MakeCache.pm,v 1.2 2005/07/19 07:19:27 marcus Exp $
 #
 
 package MakeCache;
@@ -31,190 +31,184 @@ use strict;
 
 # a list of variables that we pull from the port Makefile
 our @makeTargets = (
-    'PKGNAME',
-    'IGNORE',
-    'NO_PACKAGE',
-    'FORBIDDEN',
-    'EXTRACT_DEPENDS',
-    'PATCH_DEPENDS',
-    'FETCH_DEPENDS',
-    'BUILD_DEPENDS',
-    'LIB_DEPENDS',
-    'RUN_DEPENDS',
-    'DEPENDS',
-    'MAINTAINER',
-    'COMMENT',
+        'PKGNAME',         'IGNORE',
+        'NO_PACKAGE',      'FORBIDDEN',
+        'EXTRACT_DEPENDS', 'PATCH_DEPENDS',
+        'FETCH_DEPENDS',   'BUILD_DEPENDS',
+        'LIB_DEPENDS',     'RUN_DEPENDS',
+        'DEPENDS',         'MAINTAINER',
+        'COMMENT',
 );
 
 # Create a new cache object
 sub new {
-    my $name = shift;
-    my $self = bless {
-	CACHE   => undef,
-	SEEN    => undef,
-	BASEDIR => shift,
-    }, $name;
+        my $name = shift;
+        my $self = bless {
+                CACHE   => undef,
+                SEEN    => undef,
+                BASEDIR => shift,
+        }, $name;
 
-    $self;
+        $self;
 }
 
 # A wrapper around make(1) in the port directory, if the cache object
 # is present, simply return, otherwise pull all the requested variables
 # into the cache
 sub _execMake {
-    my $self = shift;
-    my $port = shift;
-    my @ret;
-    my $tmp;
+        my $self = shift;
+        my $port = shift;
+        my @ret;
+        my $tmp;
 
-    return if ($self->{SEEN}->{$port} eq 1);
+        return if ($self->{SEEN}->{$port} eq 1);
 
-    $tmp = '-V ' . join(' -V ', @makeTargets);
-    my $dir = $self->{BASEDIR} . '/' . $port;
-    @ret = split("\n", `cd $dir && make $tmp`);
+        $tmp = '-V ' . join(' -V ', @makeTargets);
+        my $dir = $self->{BASEDIR} . '/' . $port;
+        @ret = split("\n", `cd $dir && make $tmp`);
 
-    foreach $tmp (@makeTargets) {
-	$self->{CACHE}->{$port}{$tmp} = shift @ret;
-    }
-    $self->{SEEN}->{$port} = 1;
+        foreach $tmp (@makeTargets) {
+                $self->{CACHE}->{$port}{$tmp} = shift @ret;
+        }
+        $self->{SEEN}->{$port} = 1;
 }
 
 # Internal function for returning a port variable
 sub _getVariable {
-    my $self = shift;
-    my $port = shift;
-    my $var  = shift;
+        my $self = shift;
+        my $port = shift;
+        my $var  = shift;
 
-    $self->_execMake($port);
-    return $self->{CACHE}->{$port}{$var};
+        $self->_execMake($port);
+        return $self->{CACHE}->{$port}{$var};
 }
 
 # Internal function for returning a port dependency list
 sub _getList {
-    my $self = shift;
-    my $port = shift;
-    my $item = shift;
-    my @deps;
+        my $self = shift;
+        my $port = shift;
+        my $item = shift;
+        my @deps;
 
-    $self->_execMake($port);
-    foreach my $dep (split(/\s+/, $self->{CACHE}->{$port}{$item})) {
-	my ($d, $ddir) = split(/:/, $dep);
-	$ddir =~ s|^$self->{BASEDIR}/||;
-	push @deps, $ddir;
-    }
-    return @deps;
+        $self->_execMake($port);
+        foreach my $dep (split(/\s+/, $self->{CACHE}->{$port}{$item})) {
+                my ($d, $ddir) = split(/:/, $dep);
+                $ddir =~ s|^$self->{BASEDIR}/||;
+                push @deps, $ddir;
+        }
+        return @deps;
 }
 
 # Package name
 sub PkgName {
-    my $self = shift;
-    my $port = shift;
-    return $self->_getVariable($port, 'PKGNAME');
+        my $self = shift;
+        my $port = shift;
+        return $self->_getVariable($port, 'PKGNAME');
 }
 
 # Port comment
 sub Comment {
-    my $self = shift;
-    my $port = shift;
-    return $self->_getVariable($port, 'COMMENT');
+        my $self = shift;
+        my $port = shift;
+        return $self->_getVariable($port, 'COMMENT');
 }
 
 # Port maintainer
 sub Maintainer {
-    my $self = shift;
-    my $port = shift;
-    return $self->_getVariable($port, 'MAINTAINER');
+        my $self = shift;
+        my $port = shift;
+        return $self->_getVariable($port, 'MAINTAINER');
 }
 
 # Extract dependencies
 sub ExtractDepends {
-    my $self = shift;
-    my $port = shift;
-    return $self->_getList($port, 'EXTRACT_DEPENDS');
+        my $self = shift;
+        my $port = shift;
+        return $self->_getList($port, 'EXTRACT_DEPENDS');
 }
 
 # Patch dependencies
 sub PatchDepends {
-    my $self = shift;
-    my $port = shift;
-    return $self->_getList($port, 'PATCH_DEPENDS');
+        my $self = shift;
+        my $port = shift;
+        return $self->_getList($port, 'PATCH_DEPENDS');
 }
 
 # Fetch dependencies
 sub FetchDepends {
-    my $self = shift;
-    my $port = shift;
-    return $self->_getList($port, 'FETCH_DEPENDS');
+        my $self = shift;
+        my $port = shift;
+        return $self->_getList($port, 'FETCH_DEPENDS');
 }
 
 # Build dependencies
 sub BuildDepends {
-    my $self = shift;
-    my $port = shift;
-    return $self->_getList($port, 'BUILD_DEPENDS');
+        my $self = shift;
+        my $port = shift;
+        return $self->_getList($port, 'BUILD_DEPENDS');
 }
 
 # Library dependencies
 sub LibDepends {
-    my $self = shift;
-    my $port = shift;
-    return $self->_getList($port, 'LIB_DEPENDS');
+        my $self = shift;
+        my $port = shift;
+        return $self->_getList($port, 'LIB_DEPENDS');
 }
 
 # Run dependencies
 sub RunDepends {
-    my $self = shift;
-    my $port = shift;
-    return $self->_getList($port, 'RUN_DEPENDS');
+        my $self = shift;
+        my $port = shift;
+        return $self->_getList($port, 'RUN_DEPENDS');
 }
 
 # Other dependencies
 sub Depends {
-    my $self = shift;
-    my $port = shift;
-    return $self->_getList($port, 'DEPENDS');
+        my $self = shift;
+        my $port = shift;
+        return $self->_getList($port, 'DEPENDS');
 }
 
 # A close approximation to the 'ignore-list' target
 sub IgnoreList {
-    my $self = shift;
-    my $port = shift;
+        my $self = shift;
+        my $port = shift;
 
-    my $n = 0;
-    $self->_execMake($port);
-    foreach my $var ('NO_PACKAGE', 'IGNORE', 'FORBIDDEN') {
-	$n++ if ($self->{CACHE}->{$port}{$var} ne "");
-    }
-    return $n eq 0 ? "" : $self->PkgName($port);
+        my $n = 0;
+        $self->_execMake($port);
+        foreach my $var ('NO_PACKAGE', 'IGNORE', 'FORBIDDEN') {
+                $n++ if ($self->{CACHE}->{$port}{$var} ne "");
+        }
+        return $n eq 0 ? "" : $self->PkgName($port);
 }
 
 # A close approximation to the 'build-depends-list' target
 sub BuildDependsList {
-    my $self = shift;
-    my $port = shift;
+        my $self = shift;
+        my $port = shift;
 
-    my @deps;
-    push(@deps, $self->ExtractDepends($port));
-    push(@deps, $self->PatchDepends($port));
-    push(@deps, $self->FetchDepends($port));
-    push(@deps, $self->BuildDepends($port));
-    push(@deps, $self->LibDepends($port));
-    push(@deps, $self->Depends($port));
+        my @deps;
+        push(@deps, $self->ExtractDepends($port));
+        push(@deps, $self->PatchDepends($port));
+        push(@deps, $self->FetchDepends($port));
+        push(@deps, $self->BuildDepends($port));
+        push(@deps, $self->LibDepends($port));
+        push(@deps, $self->Depends($port));
 
-    my %uniq;
-    return grep { !$uniq{$_}++ } @deps;
+        my %uniq;
+        return grep { !$uniq{$_}++ } @deps;
 }
 
 # A close approximation to the 'run-depends-list' target
 sub RunDependsList {
-    my $self = shift;
-    my $port = shift;
+        my $self = shift;
+        my $port = shift;
 
-    my @deps;
-    push(@deps, $self->LibDepends($port));
-    push(@deps, $self->RunDepends($port));
-    push(@deps, $self->Depends($port));
+        my @deps;
+        push(@deps, $self->LibDepends($port));
+        push(@deps, $self->RunDepends($port));
+        push(@deps, $self->Depends($port));
 
-    my %uniq;
-    return grep { !$uniq{$_}++ } @deps;
+        my %uniq;
+        return grep { !$uniq{$_}++ } @deps;
 }
