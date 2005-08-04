6b7528f18c4c8d95b6df2a7cb39d895d0528b5b4
marcus <marcus>
2005-08-04 21:40:47 +0000

    Create an update schema to move from dsversion 2.0.0 to 2.1.0.

---
+++ upgrade/mig_mysql_tinderbox-2.0.0_to_2.1.0.sql
@@ -0,0 +1,8 @@
+SET FOREIGN_KEY_CHECKS=0;
+
+ALTER TABLE users
+  CHANGE User_Password varchar(41);
+
+INSERT INTO config VALUES ('__DSVERSION__', '2.1.0', -1) ON DUPLICATE KEY UPDATE Config_Option_Value=VALUES(Config_Option_Value);
+
+SET FOREIGN_KEY_CHECKS=1;
