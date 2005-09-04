7e2dcb6885f263e60e7743169d161f78eae84e61
marcus <marcus>
2005-09-04 01:12:00 +0000

    Make sure we're setting the object key value with a valid element from the attrs hash.

--- lib/Tinderbox/TinderObject.pm
+++ lib/Tinderbox/TinderObject.pm
@@ -23,7 +23,7 @@
 # OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 # SUCH DAMAGE.
 #
-# $MCom: portstools/tinderbox/lib/Tinderbox/TinderObject.pm,v 1.9 2005/09/04 00:58:15 marcus Exp $
+# $MCom: portstools/tinderbox/lib/Tinderbox/TinderObject.pm,v 1.10 2005/09/04 01:12:00 marcus Exp $
 #
 
 package TinderObject;
@@ -39,23 +39,16 @@ sub new {
         my $attrs = $args[1];
 
         my $self = {_object_hash => $args[0], _id_field => undef,};
-        foreach my $key (keys %{$self->{'_object_hash'}}) {
-                if ($key eq 'ports_tree_cvsweb_url') {
-                        $key = 'Ports_Tree_CVSweb_URL';
-                } else {
-                        $key = ucfirst $key;
-                        $key =~ s/_(.)/_\u$1/g;
-                }
-        }
         foreach my $key (keys %{$attrs}) {
-                if ($key eq 'ports_tree_cvsweb_url') {
-                        $key = 'Ports_Tree_CVSweb_URL';
+		my $skey = $key;
+                if ($skey eq 'ports_tree_cvsweb_url') {
+                        $skey = 'Ports_Tree_CVSweb_URL';
                 } else {
-                        $key = ucfirst $key;
-                        $key =~ s/_(.)/_\u$1/g;
+                        $skey = ucfirst $skey;
+                        $skey =~ s/_(.)/_\u$1/g;
                 }
-                $self->{$key} = $attrs->{$key}
-                    if (defined($self->{'_object_hash'}->{$key}));
+                $self->{$skey} = $attrs->{$key}
+                    if (defined($self->{'_object_hash'}->{$skey}));
         }
         bless($self, $class);
         $self;
