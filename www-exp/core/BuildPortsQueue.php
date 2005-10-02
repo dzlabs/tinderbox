eaae9051624ec158b2f8264c751c9492c97c4021
marcus <marcus>
2005-10-02 20:50:07 +0000

    Correct a typo that resulted in BuildPortsQueue->getEmailOnCompletion() to return null.
    Reported by:	killfill on #freebsd-gnome

--- www-exp/core/BuildPortsQueue.php
+++ www-exp/core/BuildPortsQueue.php
@@ -24,7 +24,7 @@
 # OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 # SUCH DAMAGE.
 #
-# $MCom: portstools/tinderbox/www-exp/core/BuildPortsQueue.php,v 1.5 2005/09/04 02:54:47 marcus Exp $
+# $MCom: portstools/tinderbox/www-exp/core/BuildPortsQueue.php,v 1.6 2005/10/02 20:50:07 marcus Exp $
 #
 
     require_once 'TinderObject.php';
@@ -65,7 +65,7 @@
 	}
 
 	function getEmailOnCompletion() {
-	    return $this->truth_array[$this->Email_On_Completion];
+	    return $this->_truth_array[$this->Email_On_Completion];
 	}
 
 	function getStatus() {
