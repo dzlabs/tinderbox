edad795dc205e2e68e0e0913d2641ffd9807fa7e
marcus <marcus>
2008-09-05 21:51:45 +0000

    Merge the message from Tinderbox 2.x when the tinderd queue is empty.
    Reported by:	makc

--- lib/tc_command.pl
+++ lib/tc_command.pl
@@ -24,7 +24,7 @@
 # OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 # SUCH DAMAGE.
 #
-# $MCom: portstools/tinderbox/lib/tc_command.pl,v 1.150 2008/08/15 17:23:07 marcus Exp $
+# $MCom: portstools/tinderbox/lib/tc_command.pl,v 1.151 2008/09/05 21:51:45 marcus Exp $
 #
 
 my $pb;
@@ -1668,7 +1668,7 @@ sub listBuildPortsQueue {
                             . "\n");
         } else {
                 cleanup($ds, 1,
-                        "There is no BuildPortsQueue configured in the datastore.\n"
+                        "There are no more queued ports for this host in the datastore.\n"
                 );
         }
 }
