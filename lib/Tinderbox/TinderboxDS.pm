4df98f37a6fa2177a0be12afa033ee3d40ec4493
marcus <marcus>
2010-01-03 20:56:21 +0000

    Adjust the SQL query to remove all entries from a BuildPortsQueue.  This makes things more compatible.

--- lib/Tinderbox/TinderboxDS.pm
+++ lib/Tinderbox/TinderboxDS.pm
@@ -23,7 +23,7 @@
 # OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 # SUCH DAMAGE.
 #
-# $MCom: portstools/tinderbox/lib/Tinderbox/TinderboxDS.pm,v 1.93 2009/10/17 07:23:35 marcus Exp $
+# $MCom: portstools/tinderbox/lib/Tinderbox/TinderboxDS.pm,v 1.94 2010/01/03 20:56:21 marcus Exp $
 #
 
 package Tinderbox::TinderboxDS;
@@ -392,6 +392,26 @@ sub getPortsForBuild {
         @ports = $self->_newFromArray("Port", @results);
 
         return @ports;
+}
+
+sub getBuildMaxSize {
+        my $self  = shift;
+        my $build = shift;
+        my $size;
+
+        my @results;
+        my $rc = $self->_doQueryHashRef(
+                "SELECT MAX(total_size) AS size FROM build_ports WHERE build_id=?",
+                \@results, $build->getId()
+        );
+
+        if (!$rc) {
+                return -1;
+        }
+
+        $size = $results[0]->{'size'};
+
+        return $size;
 }
 
 sub getPortById {
@@ -1529,7 +1549,7 @@ sub removeBuildPortsQueue {
         my $self = shift;
 
         my $rc;
-        $rc = $self->_doQuery("DELETE FROM build_ports_queue WHERE 1");
+        $rc = $self->_doQuery("DELETE FROM build_ports_queue");
         return $rc;
 }
 
