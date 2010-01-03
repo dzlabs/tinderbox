20489bd7dd035206fd0fd61c9cbe8df76a46634e
marcus <marcus>
2010-01-03 20:58:22 +0000

    Forced commit to note the SQL problem was reported by olli hauer <ohauer@gmx.de>.

--- lib/Tinderbox/TinderboxDS.pm
+++ lib/Tinderbox/TinderboxDS.pm
@@ -23,7 +23,7 @@
 # OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 # SUCH DAMAGE.
 #
-# $MCom: portstools/tinderbox/lib/Tinderbox/TinderboxDS.pm,v 1.95 2010/01/03 20:57:42 marcus Exp $
+# $MCom: portstools/tinderbox/lib/Tinderbox/TinderboxDS.pm,v 1.96 2010/01/03 20:58:22 marcus Exp $
 #
 
 package Tinderbox::TinderboxDS;
